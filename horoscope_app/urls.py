2af0c64221919ea31bd33f43f979b5faefae8a30
Aleksey G <alekseygalkovich@gmail.com>
2021-12-13 15:55:56 +0200

    2.7 Конвертеры роутов в Django

--- horoscope_app/urls.py
+++ horoscope_app/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<sign_horoscope>/', views.get_info_sign_horoscope),
-    # path('leo/', views.leo),
-    # path('scorpio/', views.scorpio),
-    # path('aries/', views.aries),
-    # path('taurus/', views.taurus),
+    path('<int:sign_horoscope>/', views.get_info_sign_horoscope_by_number),
+    path('<str:sign_horoscope>/', views.get_info_sign_horoscope),
 ]
--- week_days_app/views.py
+++ week_days_app/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+wd = {
+    'monday': 'понедельник',
+    'tuesday': 'вторник',
+    'wednesday': 'среда',
+    'thursday': 'четверг',
+    'friday': 'пятница',
+    'saturday': 'суббота',
+    'sunday': 'воскресенье',
+}
 
-def get_week_day(requests, week_day):
-    wd = {
-        'monday': 'понедельник',
-        'tuesday': 'вторник',
-        'wednesday': 'среда',
-        'thursday': 'четверг',
-        'friday': 'пятница',
-        'saturday': 'суббота',
-        'sunday': 'воскресенье',
-    }
+wd_list = [
+    'понедельник',
+    'вторник',
+    'среда',
+    'четверг',
+    'пятница',
+    'суббота',
+    'воскресенье',
+]
+
+
+def get_week_day(requests, week_day: str):
     if week_day in wd:
         return HttpResponse(f'Список дел, на {wd[week_day]}.')
     else:
         return HttpResponseNotFound(f'Нет дня недели: {week_day}.')
+
+
+def get_week_day_by_number(requests, week_day: int):
+    week_day -= 1
+    if abs(week_day) < 7:
+        return HttpResponse(f'День недели #{week_day + 1}: {wd_list[week_day]}.')
+    else:
+        return HttpResponseNotFound(f'Нет дня недели под номером: {week_day + 1}.')
 
 
