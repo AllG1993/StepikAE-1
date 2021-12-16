016f76976e3c9a4f6ec7a25c2e5fb6a3b9ea1eb8
Aleksey G <alekseygalkovich@gmail.com>
2021-12-16 14:33:20 +0200

    2.11 Итоги модуля. Практическое задание

--- horoscope_app/urls.py
+++ horoscope_app/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('type', views.sign_type_list, name='type'),
+    path('type/<str:sign_type>', views.sign_type_page, name='sign_type_page'),
     path('<int:sign_horoscope>', views.get_info_sign_horoscope_by_number),
     path('<str:sign_horoscope>', views.get_info_sign_horoscope, name='horoscope_name'),
+    path('<int:day>/<int:mo>', views.determining_horoscope_sign, name='determining_horoscope_sign'),
+
 ]
+
--- horoscope_app/views.py
+++ horoscope_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+
+import datetime
+
+
 sh = {"aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля)",
       "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая)",
       "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня)",
@@ -15,6 +19,13 @@ sh = {"aries": "Овен - первый знак зодиака, планета 
       "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)"
       }
 
+sign_types_dict = {
+    'fire': ['aries', 'leo', 'sagittarius'],
+    'earth': ['taurus', 'virgo', 'capricorn'],
+    'air': ['gemini', 'libra', 'aquarius'],
+    'water': ['cancer', 'scorpio', 'pisces'],
+}
+
 
 def index(request):
     sh_key_list = list(sh)
@@ -23,6 +34,24 @@ def index(request):
         sign_url = reverse('horoscope_name', args=[sign])
         result += f'<li><a href="{sign_url}">{sign.title()}</a></li>'
     return HttpResponse(f'<ol>{result}</ol>')
+
+
+def sign_type_list(request):
+    result = ''
+    for element in list(sign_types_dict):
+        url = reverse('sign_type_page', args=[element])
+        result += f'<a href="{url}"><li>{element.title()}</li></a>'
+    return HttpResponse(f'<ul>{result}</ul>')
+
+
+def sign_type_page(request, sign_type):
+    result = 'Нет такой стихии.'
+    if sign_type in sign_types_dict:
+        result = ''
+        for st in sign_types_dict[sign_type]:
+            sign_url = reverse('horoscope_name', args=[st])
+            result += f'<a href="{sign_url}"><li>{st.title()} ({sh[st].split()[0]})</a></li>'
+    return HttpResponse(f'<ul>{result}</ul>')
 
 
 def get_info_sign_horoscope(requests, sign_horoscope: str):
@@ -39,5 +68,41 @@ def get_info_sign_horoscope_by_number(requests, sign_horoscope: int):
     name_sh = sh_key_list[sign_horoscope - 1]
     redirect_url = reverse('horoscope_name', args=(name_sh,))
     return HttpResponseRedirect(redirect_url)
+
+
+def determining_horoscope_sign(request, day: int, mo: int):
+    result = HttpResponseNotFound('Некорректная дата.')
+    if mo > 12 or day > 31:
+        return result
+    else:
+        if (day >= 21 and mo == 3) or (day <= 20 and mo == 4):
+            result = 'aries'
+        elif (day >= 21 and mo == 4) or (day <= 21 and mo == 5):
+            result = 'taurus'
+        elif (day >= 22 and mo == 5) or (day <= 21 and mo == 6):
+            result = 'gemini'
+        elif (day >= 22 and mo == 6) or (day <= 22 and mo == 7):
+            result = 'cancer'
+        elif (day >= 23 and mo == 7) or (day <= 23 and mo == 8):
+            result = 'leo'
+        elif (day >= 24 and mo == 8) or (day <= 23 and mo == 9):
+            result = 'virgo'
+        elif (day >= 24 and mo == 9) or (day <= 23 and mo == 10):
+            result = 'libra'
+        elif (day >= 24 and mo == 10) or (day <= 22 and mo == 11):
+            result = 'scorpio'
+        elif (day >= 23 and mo == 11) or (day <= 21 and mo == 12):
+            result = 'sagittarius'
+        elif (day >= 22 and mo == 12) or (day <= 20 and mo == 1):
+            result = 'capricorn'
+        elif (day >= 21 and mo == 1) or (day <= 18 and mo == 2):
+            result = 'aquarius'
+        elif (day >= 19 and mo == 2) or (day <= 20 and mo == 3):
+            result = 'pisces'
+
+        url_sign = reverse('horoscope_name', args=[result])
+
+        return HttpResponse(f'День: {day} Месяц: {mo} соответствует знаку - '
+                            f'<a href="{url_sign}">{sh[result].split()[0]}</a>')
 
 
